e3ed581cb9717017ba1aafa64e6ab3f245fedee4
Zachary Michaels <mikezackles@gmail.com>
2016-06-29 17:25:00 -0400

    Remove archive sketch for now

--- test/datapack.cpp
+++ test/datapack.cpp
@@ -12,19 +12,6 @@ NOCOPY_ARRAY(coords, uint8_t, 10);
 NOCOPY_ARRAY(locations, uint32_t, 20);
 using measurement = nocopy::datapack<delta, first, second, coords, locations>;
 
-//template <std::size_t Version>
-//using measurement =
-////  field removed in this version
-//// field added in this version  |
-//nocopy::archive< //          |  |
-//  Version //                 v  v
-//, nocopy::v< delta,          0    >
-//, nocopy::v< first,          0, 1 >
-//, nocopy::v< second,         0    >
-//, nocopy::v< coords,         0    >
-//, nocopy::v< locations,      1    >
-//>;
-
 SCENARIO("datapack") {
   GIVEN("an initialized datapack") {
     measurement measured{};
