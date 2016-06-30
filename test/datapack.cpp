9e67635c0091596ee0739edbe462432e2c97b5cd
Zachary Michaels <mikezackles@gmail.com>
2016-06-30 17:18:41 -0400

    Better field namespacing in tests

--- test/datapack.cpp
+++ test/datapack.cpp
@@ -4,26 +4,34 @@
 
 namespace hana = boost::hana;
 
-namespace nocopy_measurement {
+namespace measurement_fields {
   NOCOPY_FIELD(delta, float);
   NOCOPY_FIELD(first, uint32_t);
   NOCOPY_FIELD(second, uint8_t);
   NOCOPY_ARRAY(third, int8_t, 4);
   NOCOPY_ARRAY(coords, uint8_t, 10);
   NOCOPY_ARRAY(locations, uint32_t, 20);
-  using measurement = nocopy::datapack<delta, first, second, coords, locations>;
 }
+using measurement = nocopy::datapack<
+  measurement_fields::delta
+, measurement_fields::first
+, measurement_fields::second
+, measurement_fields::coords
+, measurement_fields::locations
+>;
 
-namespace nocopy_experiment {
-  using namespace nocopy_measurement;
+namespace experiment_fields {
   NOCOPY_FIELD(measure1, measurement);
   NOCOPY_ARRAY(more_measurements, measurement, 5);
-  using experiment = nocopy::datapack<measure1, more_measurements>;
 }
+using experiment = nocopy::datapack<
+  experiment_fields::measure1
+, experiment_fields::more_measurements
+>;
 
 SCENARIO("datapack") {
   GIVEN("a datapack-sized buffer of garbage data") {
-    using namespace nocopy_measurement;
+    using namespace measurement_fields;
     std::array<uint8_t, sizeof(measurement)> buffer;
     std::fill_n(buffer.begin(), buffer.size(), 1);
 
@@ -53,7 +61,7 @@ SCENARIO("datapack") {
   }
 
   GIVEN("an initialized datapack") {
-    using namespace nocopy_measurement;
+    using namespace measurement_fields;
     measurement measured{};
     measured.set<delta>(0.5);
     measured.set<first>(1001);
