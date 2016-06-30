fbc156ae8cd5578eaed25afb3afb8527e57f99d5
Zachary Michaels <mikezackles@gmail.com>
2016-06-30 17:01:27 -0400

    Runtime alignment checks for arraypack

--- include/nocopy/arraypack.hpp
+++ include/nocopy/arraypack.hpp
@@ -2,20 +2,29 @@
 #define UUID_E7D186DD_98F4_410C_A45A_9C39E7680281
 
 #include "detail/field_accessors.hpp"
+#include "detail/traits.hpp"
+
+#include <array>
 
 namespace nocopy {
   template <typename T, std::size_t Size>
   class arraypack {
   public:
     decltype(auto) get(std::size_t index) {
+      assert(reinterpret_cast<std::uintptr_t>(this) % detail::find_alignment<T>::result == 0);
+
       return detail::field_getter<T>::get(buffer_[index]);
     }
 
     decltype(auto) get(std::size_t index) const {
+      assert(reinterpret_cast<std::uintptr_t>(this) % detail::find_alignment<T>::result == 0);
+
       return detail::field_getter<T const>::get(buffer_[index]);
     }
 
     void set(std::size_t index, T val) {
+      assert(reinterpret_cast<std::uintptr_t>(this) % detail::find_alignment<T>::result == 0);
+
       return detail::field_setter<T>::set(buffer_[index], val);
     }
 
