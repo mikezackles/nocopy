ca87f21a802693297d908861a04fca3b998d3db5
Zachary Michaels <mikezackles@gmail.com>
2016-07-05 08:44:52 -0400

    Remove dead code

--- include/nocopy.hpp
+++ include/nocopy.hpp
@@ -2,6 +2,7 @@
 #define UUID_B6D3A61F_F9B9_4F5D_8899_349259B6DFE4
 
 #include "nocopy/archive.hpp"
+#include "nocopy/box.hpp"
 #include "nocopy/datapack.hpp"
 #include "nocopy/field.hpp"
 #include "nocopy/version_range.hpp"
--- include/nocopy/field.hpp
+++ include/nocopy/field.hpp
@@ -1,8 +1,6 @@
 #ifndef UUID_CA2A183A_D196_4445_B344_55ACD2FB9B0E
 #define UUID_CA2A183A_D196_4445_B344_55ACD2FB9B0E
 
-#include "box.hpp"
-#include "detail/traits.hpp"
 #include "static_asserts.hpp"
 
 namespace nocopy {
@@ -10,15 +8,6 @@ namespace nocopy {
   struct multi {
     static_assert(Count > 0, "multi field must have count greater than 0");
   };
-
-  //template <typename T>
-  //struct field {
-  //  //using type = T;
-  //  //using return_type = detail::find_return_type_t<T>;
-  //  //static constexpr auto name() { return "unnamed"; }
-  //  //static constexpr std::size_t alignment() { return detail::find_alignment_c<FieldType>; }
-  //  //static constexpr std::size_t size() { return sizeof(return_type); }
-  //};
 }
 
 #ifndef NOCOPY_NO_MACROS
