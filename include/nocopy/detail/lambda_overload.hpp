25a43dd36996bf8627d969a48ea3ad2a1b5b5115
Zachary Michaels <mikezackles@gmail.com>
2016-07-08 11:36:49 -0400

    Fix lambda overload ambiguity

--- include/nocopy/detail/lambda_overload.hpp
+++ include/nocopy/detail/lambda_overload.hpp
@@ -2,9 +2,18 @@
 #define UUID_AA9FDD71_5F9D_4874_B5E2_888E951DB878
 
 namespace nocopy { namespace detail {
-  template <typename ...Lambdas>
-  struct lambda_overload : Lambdas... {
-    lambda_overload(Lambdas... lambdas) : Lambdas{std::move(lambdas)}... {}
+  template <typename Lambda, typename ...Lambdas>
+  struct lambda_overload : Lambda, lambda_overload<Lambdas...> {
+    using Lambda::operator();
+    using lambda_overload<Lambdas...>::operator();
+    lambda_overload(Lambda lambda, Lambdas... lambdas)
+      : Lambda{std::move(lambda)}, lambda_overload<Lambdas...>{std::move(lambdas)...} {}
+  };
+
+  template <typename Lambda>
+  struct lambda_overload<Lambda> : Lambda {
+    using Lambda::operator();
+    lambda_overload(Lambda lambda) : Lambda{std::move(lambda)} {}
   };
 }}
 
